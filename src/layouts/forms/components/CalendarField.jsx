8a240dae4049f9d9639ee00ee57e6bab29bd6350
Ricardo Cordovilla <101917118+RicardoCordovilla@users.noreply.github.com>
2024-10-30 14:02:31 -0500

    Update CalendarField.jsx

--- src/layouts/forms/components/CalendarField.jsx
+++ src/layouts/forms/components/CalendarField.jsx
@@ -19,7 +19,7 @@ export { Calendar };
 const CalendarField = ({ fecha, setFecha }) => {
 
     const [data, loding, error, fetch] = useFetch()
-    // const [disabledDays, setDisabledDays] = useState([])
+    const [disabledDays, setDisabledDays] = useState([])
     const [enabledDays, setEnabledDays] = useState([4,5,6])
     const [enabledDates, setEnabledDates] = useState([])
     const [disabledDates, setDisabledDates] = useState([])
@@ -33,7 +33,7 @@ const CalendarField = ({ fecha, setFecha }) => {
         const response = data && data[0].data
         if (!data) return
         // console.log(response?.calendar)
-        // setEnabledDays(response?.weekdays)
+        setEnabledDays([4,5,6])
         console.log(response?.weekdays)
         setDisabledDates([...disabledDates, new Date(response?.calendar.disable)])
         setEnabledDates([...enabledDates, new Date(response?.calendar.enable)])
